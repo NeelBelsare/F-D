9318a84af018830695d7c0f5e83af4434717cf4a
Neel <neel.belsare@mgmtech.org>
2026-07-21 12:45:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // 1. RENDER API CONFIGURATION
 // ==========================================
 // CHANGE THIS to your deployed Render URL before pushing to GitHub:
-const BACKEND_URL = 'https://f-d-1.onrender.com'; 
+const BACKEND_URL = 'dep-d9fhlujrjlhs73ac269g'; 
 
 let activeCaseId = null;
 let currentUserRole = 'Officer';
